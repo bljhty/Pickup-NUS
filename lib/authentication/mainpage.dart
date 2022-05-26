43d9bf9b66c7d40e106ca96314e305e21a57e8d6
thomasjlalba <e0773507@u.nus.edu>
2022-05-26 15:11:54 +0800

    added register user feature

--- lib/authentication/mainpage.dart
+++ lib/authentication/mainpage.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:orbital_nus/login_screen.dart';
-import 'package:orbital_nus/userhomepage.dart';
+import 'package:orbital_nus/authentication/pages/login_screen.dart';
+import 'package:orbital_nus/authentication/pages/userhomepage.dart';
+
+import 'auth_page.dart';
 
 // MainPage checks if user has already been logged in upon starting the app
 class MainPage extends StatelessWidget {
@@ -14,9 +16,9 @@ class MainPage extends StatelessWidget {
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return UserHomePage();
+            return const UserHomePage();
             } else {
-            return LoginPage();
+            return const AuthPage();
           }
     }
     ),

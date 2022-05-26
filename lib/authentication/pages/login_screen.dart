d7cd561fd5ae7dbb6b63e4527107bda7b028c4e5
Bryan Lee <bljhty>
2022-05-26 17:45:08 +0800

    data base added

--- lib/authentication/pages/login_screen.dart
+++ lib/authentication/pages/login_screen.dart
@@ -5,6 +5,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'forgot_password_page.dart';
+
 class LoginPage extends StatefulWidget {
   final VoidCallback showRegisterPage;
   const LoginPage({Key? key, required this.showRegisterPage}) : super(key: key);
@@ -14,7 +16,6 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
   // text controllers
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
@@ -134,7 +135,6 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                   ),
 
-
                   SizedBox(height: 10),
 
                   // sign in button
@@ -173,9 +173,9 @@ class _LoginPageState extends State<LoginPage> {
                           color: Colors.white,
                         ),
                       ),
-
                       GestureDetector(
-                        onTap: widget.showRegisterPage, // directs user to a sign up page
+                        onTap: widget
+                            .showRegisterPage, // directs user to a sign up page
                         child: Text(
                           ' Sign up now!',
                           style: TextStyle(

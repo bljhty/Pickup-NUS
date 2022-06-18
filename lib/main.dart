9b9bfe622e10dfa3dacad5c3638fa7ea04c8e7a7
Bryan Lee <bljhty>
2022-06-18 09:55:43 +0800

    splash screen added, merchant register page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 import 'package:orbital_nus/authentication/mainpage.dart';
+import 'package:orbital_nus/authentication/pages/splash_page.dart';
 import 'authentication/pages/login_screen.dart';
-
 
 void main() async {
   // Initialise firebase as a database
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MainPage(),
+      home: Splashscreen(),
     );
   }
 }

9758f40078fd6993fafb3d54f53a70eb02f06c64
thomasjlalba <e0773507@u.nus.edu>
2022-06-25 14:11:38 +0800

    added merchant order history

--- lib/Merchant side/Profile/merchant_profile_page.dart
+++ lib/Merchant side/Profile/merchant_profile_page.dart
@@ -3,6 +3,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:orbital_nus/Merchant%20side/Merchant%20Bottom%20Bar/merchant_bottom_bar.dart';
+import 'package:orbital_nus/Merchant%20side/Merchant%20Home%20Page/Past%20Orders/merchant_past_orders_page.dart';
 import 'package:orbital_nus/authentication/mainpage.dart';
 import 'package:orbital_nus/colors.dart';
 
@@ -18,7 +19,8 @@ class MerchantProfilePage extends StatelessWidget {
         centerTitle: true,
         title: const Text('My Profile'),
       ),
-      bottomNavigationBar: const MerchantBottomBar(selectMenu: MenuState.profile),
+      bottomNavigationBar:
+          const MerchantBottomBar(selectMenu: MenuState.profile),
       backgroundColor: kBackgroundColor,
       body: SafeArea(
         child: Center(
@@ -26,21 +28,28 @@ class MerchantProfilePage extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                const SizedBox(height: 10,),
+                const SizedBox(
+                  height: 10,
+                ),
                 Image.asset(
                   'assets/images/Logo.png',
-                      height: 150,
+                  height: 150,
                   width: 150,
                   color: kPrimaryColor,
                 ),
-                const SizedBox(height: 30,),
+                const SizedBox(
+                  height: 30,
+                ),
 
                 // Past Orders
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 25.0),
                   child: GestureDetector(
                     onTap: () {
-
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) {
+                        return const MerchantPastOrdersPage();
+                      }));
                     },
                     child: Container(
                       padding: const EdgeInsets.all(15),
@@ -76,7 +85,7 @@ class MerchantProfilePage extends StatelessWidget {
                       ),
                       child: const Center(
                         child: Text(
-                          'Edit Menu',
+                          'Edit Menu (Coming Soon)',
                           style: TextStyle(
                             color: Colors.white,
                             fontWeight: FontWeight.bold,
@@ -87,7 +96,9 @@ class MerchantProfilePage extends StatelessWidget {
                     ),
                   ),
                 ),
-                const SizedBox(height: 30,),
+                const SizedBox(
+                  height: 30,
+                ),
 
                 // Log out button
                 Padding(
@@ -97,8 +108,8 @@ class MerchantProfilePage extends StatelessWidget {
                       FirebaseAuth.instance.signOut();
                       Navigator.push(context,
                           MaterialPageRoute(builder: (context) {
-                            return const MainPage();
-                          }));
+                        return const MainPage();
+                      }));
                     },
                     child: Container(
                       padding: const EdgeInsets.all(15),
@@ -119,7 +130,9 @@ class MerchantProfilePage extends StatelessWidget {
                     ),
                   ),
                 ),
-                const SizedBox(height: 30,),
+                const SizedBox(
+                  height: 30,
+                ),
               ],
             ),
           ),

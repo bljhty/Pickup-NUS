36b2a431d17355d08b99b59628c3e6a0a55f4f7f
thomasjlalba <e0773507@u.nus.edu>
2022-06-30 16:32:58 +0800

    started on edit menu page

---
+++ lib/Buyer Side/Profile/edit menu/models/edit_item_page.dart
@@ -0,0 +1,20 @@
+// Page for merchants to edit the information of the food item
+
+import 'package:flutter/material.dart';
+import 'package:orbital_nus/Buyer%20Side/get_information/get_food.dart';
+
+class EditItemPage extends StatefulWidget {
+  final Food food;
+
+  EditItemPage(this.food);
+
+  @override
+  State<EditItemPage> createState() => _EditItemPageState();
+}
+
+class _EditItemPageState extends State<EditItemPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

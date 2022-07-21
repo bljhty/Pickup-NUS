74169c27c7b944fa1e5b849f2ecfbe33edcff354
Bryan Lee <bljhty>
2022-07-21 14:44:02 +0800

    changes to app bar and settings page

--- lib/authentication/pages/paymentpage.dart
+++ lib/authentication/pages/paymentpage.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_typing_uninitialized_variables
-
+// no longer in used
+/*
 import 'dart:convert';
 import 'dart:developer';
 
@@ -103,3 +104,4 @@ class PaymentPage extends StatelessWidget {
     );
   }
 }
+*/
